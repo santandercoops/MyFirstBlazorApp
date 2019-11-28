24ac3c69cf256e488082791095e6299d9bbf4375
santandercoops <58299686+santandercoops@users.noreply.github.com>
2019-11-28 14:35:34 +0000

    Add files via upload

---
+++ MyFirstBlazorApp/Pages/FetchData.razor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using MyFirstBlazorApp.Data;
+
+namespace MyFirstBlazorApp.Pages
+{
+    public class FetchDataBase : ComponentBase
+    {
+        [Inject] WeatherForecastService ForecastService { get; set; }
+
+        protected WeatherForecast[] forecasts;
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
