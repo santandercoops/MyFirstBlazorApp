809ed331cad4a3c8cdc4840f2e59b7259b34e71d
santandercoops <58299686+santandercoops@users.noreply.github.com>
2019-11-28 14:36:06 +0000

    Add files via upload

---
+++ MyFirstBlazorApp.Test/CounterTests.cs
@@ -0,0 +1,28 @@
+using System;
+using Microsoft.AspNetCore.Components.Testing;
+using MyFirstBlazorApp.Pages;
+using Xunit;
+
+namespace MyFirstBlazorApp.Test
+{
+    public class CounterTests
+    {
+        //private TestHost host = new TestHost();
+
+        //[Fact]
+        //public void CounterWorks()
+        //{
+        //    var component = host.AddComponent<Counter>();
+        //    Func<string> countValue = () => component.Find("#count").InnerText;
+
+        //    Assert.Equal("Counter", component.Find("h1").InnerText);
+        //    Assert.Equal("Current count: 0", countValue());
+
+        //    component.Find("button.inc").Click();
+        //    Assert.Contains("Current count: 1", countValue());
+
+        //    component.Find("button.dec").Click();
+        //    Assert.Contains("Current count: 0", countValue());
+        //}
+    }
+}
